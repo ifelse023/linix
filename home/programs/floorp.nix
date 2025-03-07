27babe1fad0e863bd7dcc01333036020b9a94b50
ifelse023 <ifelse023@gmail.com>
2025-03-07 18:19:14 +0100

    :)

--- home/programs/floorp.nix
+++ home/programs/floorp.nix
@@ -1,10 +1,23 @@
 {
-  programs.floorp = {
+  programs.firefox = {
     enable = true;
     policies = {
       DontCheckDefaultBrowser = true;
       DisablePocket = true;
       DisableAppUpdate = true;
     };
+    profiles.default = {
+      settings = {
+        "browser.aboutConfig.showWarning" = false
+      }
   };
-}
+
+  };
+
+    services.psd = {
+      enable = true;
+      resyncTimer = "10m";
+    };
+
+  }
+
